2282d205f942b7076e75433a2939216ca42b68fa
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-02-23 14:00:48 +0900

    Initial commit

---
+++ lib/SPVM/Time/HiRes.pm
@@ -0,0 +1,45 @@
+package SPVM::Time::HiRes;
+
+our $VERSION = '0.01';
+
+1;
+
+=head1 Name
+
+SPVM::Time::HiRes - Short Description
+
+=head1 Description
+
+C<SPVM::Time::HiRes> is the C<Time::HiRes> class in L<SPVM> language.
+
+=head1 Usage
+
+  use Time::HiRes;
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright 2023-2023 Yuki Kimoto, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
