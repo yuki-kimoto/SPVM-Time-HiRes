400656cf4063c6137395a84b9f3fdfba616ecb08
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-11-27 09:42:21 +0900

    Add Time::HiRes::ItimervalFloat Time::HiRes::FloatSecond.

---
+++ lib/SPVM/Time/HiRes/ItimervalFloat.pm
@@ -0,0 +1,45 @@
+package SPVM::Time::HiRes::ItimervalFloat;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Time::HiRes::ItimervalFloat - Short Description
+
+=head1 Description
+
+The Time::HiRes::ItimervalFloat class of L<SPVM> has methods to do someting.
+
+=head1 Usage
+
+  use Time::HiRes::ItimervalFloat;
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2023 Yuki Kimoto
+
+MIT License
+
