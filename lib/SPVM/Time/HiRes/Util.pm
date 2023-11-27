8c577d187fa958074af7c2361cfb2561e5cb6649
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-11-27 15:06:13 +0900

    Improve the doc of Util.

--- lib/SPVM/Time/HiRes/Util.pm
+++ lib/SPVM/Time/HiRes/Util.pm
@@ -72,9 +72,11 @@ C<static method float_seconds_to_timespec : L<Sys::Time::Timespec|SPVM::Sys::Tim
 
 Converts floating seconds $float_seconds to a L<Sys::Time::Timespec|SPVM::Sys::Time::Timespec> object, and returns it.
 
+This method may result in a loss of precision.
+
 Exceptions:
 
-$float_seconds must be less than or equal to Fn->INT_MAX. Otherwise an exception is thrown.
+$float_seconds must be greater than or equal to 0. Otherwise an exception is thrown.
 
 =head2 timespec_to_float_seconds
 
@@ -82,9 +84,13 @@ C<static method timespec_to_float_seconds : double ($ts : L<Sys::Time::Timespec|
 
 Converts the L<Sys::Time::Timespec|SPVM::Sys::Time::Timespec> object $ts to floating seconds, and returns it.
 
+This method may result in a loss of precision.
+
 Exceptions:
 
-$ts->tv_sec must be less than or equal to Fn->INT_MAX. Otherwise an exception is thrown.
+$ts->tv_sec must be greater than or equal to 0. Otherwise an exception is thrown.
+
+$ts->tv_nsec must be greater than or equal to 0. Otherwise an exception is thrown.
 
 =head2 float_seconds_to_timeval
 
@@ -92,9 +98,11 @@ C<static method float_seconds_to_timeval : L<Sys::Time::Timeval|SPVM::Sys::Time:
 
 Converts floating seconds $float_seconds to a L<Sys::Time::Timeval|SPVM::Sys::Time::Timeval> object, and returns it.
 
+This method may result in a loss of precision.
+
 Exceptions:
 
-$float_seconds must be less than or equal to Fn->INT_MAX. Otherwise an exception is thrown.
+$float_seconds must be greater than or equal to 0. Otherwise an exception is thrown.
 
 =head2 timeval_to_float_seconds
 
@@ -102,15 +110,25 @@ C<static method timeval_to_float_seconds : double ($tv : L<Sys::Time::Timeval|SP
 
 Converts the L<Sys::Time::Timeval|SPVM::Sys::Time::Timeval> object $tv to floating seconds, and returns it.
 
+This method may result in a loss of precision.
+
+Exceptions:
+
+$tv->tv_sec must be greater than or equal to 0. Otherwise an exception is thrown.
+
+$tv->tv_usec must be greater than or equal to 0. Otherwise an exception is thrown.
+    
 =head2 float_seconds_to_nanoseconds
 
 C<static method float_seconds_to_nanoseconds : long ($float_seconds : double);>
 
 Converts floating seconds $float_seconds to nanoseconds, and returns it.
 
-Exceptions:
+This method may result in a loss of precision.
 
-$float_seconds must be less than or equal to Fn->INT_MAX. Otherwise an exception is thrown.
+Excetpions:
+
+$float_seconds must be greater than or equal to 0. Otherwise an exception is thrown.
 
 =head2 nanoseconds_to_float_seconds
 
@@ -118,11 +136,23 @@ C<static method nanoseconds_to_float_seconds : double ($nanoseconds : long);>
 
 Converts nanoseconds $nanoseconds to floating seconds, and returns it.
 
+This method may result in a loss of precision.
+
+Excetpions:
+
+$nanoseconds must be greater than or equal to 0. Otherwise an exception is thrown.
+
 =head2 float_seconds_to_microseconds
 
 C<static method float_seconds_to_microseconds : long ($float_seconds : double);>
 
 Converts floating seconds $float_seconds to microseconds, and returns it.
+
+This method may result in a loss of precision.
+
+Excetpions:
+
+$float_seconds must be greater than or equal to 0. Otherwise an exception is thrown.
 
 =head2 microseconds_to_float_seconds
 
@@ -130,17 +160,47 @@ C<static method microseconds_to_float_seconds : double ($microseconds : long);>
 
 Converts microseconds $microseconds to floating seconds, and returns it.
 
+This method may result in a loss of precision.
+
+Excetpions:
+
+$float_seconds must be greater than or equal to 0. Otherwise an exception is thrown.
+
 =head2 timeval_interval
 
 C<static method timeval_interval : double ($tv_a : L<Sys::Time::Timeval|SPVM::Sys::Time::Timeval>, $tv_b : L<Sys::Time::Timeval|SPVM::Sys::Time::Timeval>);>
 
 Calculates $tv_b - $tv_a and returns it as floating seconds.
 
+This method may result in a loss of precision.
+
+Excetpions:
+
+$tv_a->tv_sec must be greater than or equal to 0. Otherwise an exception is thrown.
+
+$tv_a->tv_usec must be greater than or equal to 0. Otherwise an exception is thrown.
+
+$tv_b->tv_sec must be greater than or equal to 0. Otherwise an exception is thrown.
+
+$tv_b->tv_usec must be greater than or equal to 0. Otherwise an exception is thrown.
+
 =head2 timespec_interval
 
 C<static method timespec_interval : double ($ts_a : L<Sys::Time::Timespec|SPVM::Sys::Time::Timespec>, $ts_b : L<Sys::Time::Timespec|SPVM::Sys::Time::Timespec>);>
 
 Calculates $ts_b - $ts_a and returns it as floating seconds.
+
+This method may result in a loss of precision.
+
+Excetpions:
+
+$ts_a->tv_sec must be greater than or equal to 0. Otherwise an exception is thrown.
+
+$ts_a->tv_nsec must be greater than or equal to 0. Otherwise an exception is thrown.
+
+$ts_b->tv_sec must be greater than or equal to 0. Otherwise an exception is thrown.
+
+$ts_b->tv_nsec must be greater than or equal to 0. Otherwise an exception is thrown.
 
 =head1 Copyright & License
 
