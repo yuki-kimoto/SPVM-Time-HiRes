7524a86efc1974c79d7032fd9469070ad7082ae6
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-11-27 11:11:52 +0900

    Add sleep, alarm.

--- lib/SPVM/Time/HiRes.pm
+++ lib/SPVM/Time/HiRes.pm
@@ -30,6 +30,45 @@ C<static method usleep : int ($usec : int);>
 
 C<static method nanosleep : long ($nanoseconds : long);>
 
+=head2 tv_interval
+
+C<static method tv_interval : double ($a : Sys::Time::Timeval, $b : Sys::Time::Timeval = undef);>
+
+=head2 time
+
+C<static method time : double ();>
+
+Exceptions:
+
+This method throws the following exception. This method is not implemented because it's clear that the lack of precision will cause future bugs.
+
+=head2 sleep
+
+C<static method sleep : double ($float_seconds : double);>
+
+=head2 alarm
+
+C<static method alarm : double ($float_seconds : double, $interval_float_seconds : double = 0);>
+
+=head2 setitimer
+
+C<static method setitimer : Time::HiRes::ItimervalFloat ($which : int, $new_itimer_float : Time::HiRes::ItimervalFloat);>
+
+=head2 getitimer
+
+C<static method getitimer : Time::HiRes::ItimervalFloat ($which : int);>
+
+=head2 clock_gettime
+
+C<static method clock_gettime : double ($clk_id : int);>
+
+=head2 clock_getres
+
+C<static method clock_getres : double ($clk_id : int);>
+
+=head2 clock_nanosleep
+
+C<static method clock_nanosleep : long ($clockid : int, $nanoseconds : long, $flags : int = 0);>
 
 =head1 Repository
 
